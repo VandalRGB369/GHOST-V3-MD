716f5744e55da7654bef53a55db171fa193e0e34
GHOST-V2-MD <madushasamanmali1985@gmail.com>
2024-09-07 09:52:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "7dclXDRI#Nqu9mIFPSSpOWDTcNY4hdDU7whNmv0xiTeR7wyQ_d3U",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
